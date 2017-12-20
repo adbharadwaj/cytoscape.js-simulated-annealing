5ab3149def3bb8270e9c843b0dd4cbb34ff00df7
Aditya Bharadwaj <adb@vt.edu>
2017-12-20 00:41:57 -0500

    Updated the list of default options and added comments

--- src/SimulatedAnnealing/base.js
+++ src/SimulatedAnnealing/base.js
@@ -1,158 +1,158 @@
-(function () {
-    "use strict";
-    window.SimulatedAnnealing = function (userConfig) {
-        var _extend = function (a, b) {
-            for (var key in b) {
-                if (b.hasOwnProperty(key)) {
-                    a[key] = b[key];
-                }
-            }
-            return a;
-        };
-        var _clone = function (obj) {
-            if(obj == null || typeof(obj) != 'object')
-                return obj;
+( function () {
+	"use strict";
+	window.SimulatedAnnealing = function ( userConfig ) {
+		var _extend = function ( a, b ) {
+			for ( var key in b ) {
+				if ( b.hasOwnProperty( key ) ) {
+					a[ key ] = b[ key ];
+				}
+			}
+			return a;
+		};
+		var _clone = function ( obj ) {
+			if ( obj == null || typeof ( obj ) != 'object' )
+				return obj;
 
-            var temp = obj.constructor(); // changed
+			var temp = obj.constructor(); // changed
 
-            for(var key in obj)
-                temp[key] = _clone(obj[key]);
-            return temp;
-        };
+			for ( var key in obj )
+				temp[ key ] = _clone( obj[ key ] );
+			return temp;
+		};
 
-        var defaultConfig = {
-            initialTemperature: 100,
-            temperatureDecreaseRate: 0.99,
-            iterations: 1,
-            steps: 10,
-            getInitialState : function (temperature) {
-                return Promise.resolve({
-                    value: temperature
-                });
-            },
-            getNeighboringState : function (state, temperature) {
-                return Promise.resolve({
-                    value: state.value + (temperature / 2 - temperature * Math.random())
-                });
-            },
-            calculateEnergy : function (state) {
-                return Promise.resolve(Math.abs(state.value));
-            },
-            terminatingCondition : function (state, temperature) {
-                return temperature === 0 || _currentIteration >= config.iterations;
-            },
-            decreaseTemperature : function (state, energy, temperature){
-                return temperature * config.temperatureDecreaseRate;
-            },
-            decideChange : function (newEnergy, oldEnergy, temperature) {
-                var delta = newEnergy - oldEnergy;
-                if (delta < 0) {
-                    return true;
-                }
+		var defaultConfig = {
+			initialTemperature: 100,
+			temperatureDecreaseRate: 0.99,
+			iterations: 1,
+			steps: 10,
+			getInitialState: function ( temperature ) {
+				return Promise.resolve( {
+					value: temperature
+				} );
+			},
+			getNeighboringState: function ( state, temperature ) {
+				return Promise.resolve( {
+					value: state.value + ( temperature / 2 - temperature * Math.random() )
+				} );
+			},
+			calculateEnergy: function ( state ) {
+				return Promise.resolve( Math.abs( state.value ) );
+			},
+			terminatingCondition: function ( state, temperature ) {
+				return temperature === 0 || _currentIteration >= config.iterations;
+			},
+			decreaseTemperature: function ( state, energy, temperature ) {
+				return temperature * config.temperatureDecreaseRate;
+			},
+			decideChange: function ( newEnergy, oldEnergy, temperature ) {
+				var delta = newEnergy - oldEnergy;
+				if ( delta < 0 ) {
+					return true;
+				}
 
-                var C = Math.exp(-delta / temperature);
-                var R = Math.random();
+				var C = Math.exp( -delta / temperature );
+				var R = Math.random();
 
-                return R < C;
-            },
-            discardChange : function (currentState, nextState) {
-                return Promise.resolve();
-            },
-            acceptChange : function (currentState, newState) {
-                _currentState = _clone(newState);
-                return Promise.resolve();
-            },
-            onStep:  function (obj) {
-                console.log(obj);
-            }
-        };
+				return R < C;
+			},
+			discardChange: function ( currentState, nextState ) {
+				return Promise.resolve();
+			},
+			acceptChange: function ( currentState, newState ) {
+				_currentState = _clone( newState );
+				return Promise.resolve();
+			},
+			onStep: function ( obj ) {
+				console.log( obj );
+			}
+		};
 
-        var config = _extend(defaultConfig, userConfig);
+		var config = _extend( defaultConfig, userConfig );
 
-        var _currentState,
-            _currentStep,
-            _currentIteration,
-            _currentEnergy,
-            _currentTemperature;
+		var _currentState,
+			_currentStep,
+			_currentIteration,
+			_currentEnergy,
+			_currentTemperature;
 
-        return {
-            doStep : function () {
-                var _nextState, _energy;
-                return config.getNeighboringState(_currentState, _currentTemperature)
-                    .then(function (nextState) {
-                        _nextState = nextState;
-                        return config.calculateEnergy(nextState)
-                    })
-                    .then(function (energy) {
-                        if (!config.decideChange(energy, _currentEnergy, _currentTemperature)) {
-                            return config.discardChange(_currentState, _nextState).then(function () {
-                                _currentStep += 1;
-                                return Promise.resolve();;
-                            });
-                        } else {
-                            _energy = energy;
-                            return config.acceptChange(_currentState, _nextState).then(function(){
-                                _currentStep += 1;
-                                _currentEnergy = _energy;
-                                return Promise.resolve();
-                            });
-                        }
-                    });
-            },
-            init : function () {
-                _currentTemperature = config.initialTemperature;
-                return config.getInitialState(_currentTemperature)
-                        .then(function (currentState) {
-                            _currentState = currentState
-                            return config.calculateEnergy(currentState);
-                        })
-                        .then(function(currentEnergy){
-                            _currentEnergy = currentEnergy;
-                            _currentIteration = 0;
-                            return Promise.resolve();
-                        });
-            },
-            doIteration: function () {
-                var sa = this;
-                return sa.doStep().then(function () {
-                    config.onStep(sa.getCurrentObject());
-                    if (_currentStep < config.steps) {
-                        return sa.doIteration();
-                    } else {
-                        return Promise.resolve();
-                    }
-                });
-            },
-            anneal: function () {
-                var sa = this;
-                _currentStep = 0;
-                return sa.doIteration().then(function () {
-                    _currentTemperature = config.decreaseTemperature(_currentState, _currentEnergy, _currentTemperature);
-                    _currentIteration += 1
-                    if (!config.terminatingCondition(_currentState, _currentTemperature)) {
-                        return sa.anneal();
-                    } else {
-                        return sa.getCurrentObject();
-                    }
-                });
-            },
-            run : function () {
-                var sa = this;
-                return sa.init().then(function () {
-                    return sa.anneal().then(function(currentObject){
-                        return currentObject;
-                    })
-                });
-            },
-            getCurrentObject : function () {
-                return {
-                    state: _currentState,
-                    energy: _currentEnergy,
-                    temperature: _currentTemperature,
-                    steps: _currentStep,
-                    iterations: _currentIteration
-                };
-            }
-        };
-    };
-})();
+		return {
+			doStep: function () {
+				var _nextState, _energy;
+				return config.getNeighboringState( _currentState, _currentTemperature )
+					.then( function ( nextState ) {
+						_nextState = nextState;
+						return config.calculateEnergy( nextState )
+					} )
+					.then( function ( energy ) {
+						if ( !config.decideChange( energy, _currentEnergy, _currentTemperature ) ) {
+							return config.discardChange( _currentState, _nextState ).then( function () {
+								_currentStep += 1;
+								return Promise.resolve();;
+							} );
+						} else {
+							_energy = energy;
+							return config.acceptChange( _currentState, _nextState ).then( function () {
+								_currentStep += 1;
+								_currentEnergy = _energy;
+								return Promise.resolve();
+							} );
+						}
+					} );
+			},
+			init: function () {
+				_currentTemperature = config.initialTemperature;
+				return config.getInitialState( _currentTemperature )
+					.then( function ( currentState ) {
+						_currentState = currentState
+						return config.calculateEnergy( currentState );
+					} )
+					.then( function ( currentEnergy ) {
+						_currentEnergy = currentEnergy;
+						_currentIteration = 0;
+						return Promise.resolve();
+					} );
+			},
+			doIteration: function () {
+				var sa = this;
+				return sa.doStep().then( function () {
+					config.onStep( sa.getCurrentObject() );
+					if ( _currentStep < config.steps ) {
+						return sa.doIteration();
+					} else {
+						return Promise.resolve();
+					}
+				} );
+			},
+			anneal: function () {
+				var sa = this;
+				_currentStep = 0;
+				return sa.doIteration().then( function () {
+					_currentTemperature = config.decreaseTemperature( _currentState, _currentEnergy, _currentTemperature );
+					_currentIteration += 1
+					if ( !config.terminatingCondition( _currentState, _currentTemperature ) ) {
+						return sa.anneal();
+					} else {
+						return sa.getCurrentObject();
+					}
+				} );
+			},
+			run: function () {
+				var sa = this;
+				return sa.init().then( function () {
+					return sa.anneal().then( function ( currentObject ) {
+						return currentObject;
+					} )
+				} );
+			},
+			getCurrentObject: function () {
+				return {
+					state: _currentState,
+					energy: _currentEnergy,
+					temperature: _currentTemperature,
+					steps: _currentStep,
+					iterations: _currentIteration
+				};
+			}
+		};
+	};
+} )();
--- src/layout.js
+++ src/layout.js
@@ -1,46 +1,75 @@
 'use strict';
 
-var _ = require('lodash');
-var async = require('async');
-var SimulatedAnnealing = require('./SimulatedAnnealing/graph.js');
+var _ = require( 'lodash' );
+var async = require( 'async' );
+var SimulatedAnnealing = require( './SimulatedAnnealing/graph.js' );
 // registers the extension on a cytoscape lib ref
 
-var SALayout = function (cytoscape) {
+var SALayout = function ( cytoscape ) {
 
-	if (!cytoscape) {
+	if ( !cytoscape ) {
 		return;
 	} // can't register if cytoscape unspecified
 
+	// Default options
 	var defaults = {
-		/*
-		 * Default configuration
-		 */
-		animate: true,
+		animate: true, // whether to animate changes to the layout
+		animationDuration: 200, // duration of animation in ms, if enabled
+		animationEasing: undefined, // easing of animation, if enabled
+		animateFilter: function ( node, i ) {
+			/*
+			A function that determines whether the node should be animated.
+			All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
+			*/
+			return true;
+		},
+		fit: false, // whether to fit the viewport to the graph. If set to true will move every node even if you move one node, because it is trying to fit the graph.
+		padding: 30, // padding to leave between graph and viewport
 		pan: {
 			x: 0,
 			y: 0,
+		}, // pan the graph to the provided position, given as { x, y }
+		ready: undefined, // callback for the layoutready event
+		stop: undefined, // callback for the layoutstop event
+		spacingFactor: 1, // a positive value which adjusts spacing between nodes (>1 means greater than usual spacing)
+		transform: function ( node, position ) {
+			/*
+			Transform a given node position. Useful for changing flow direction in discrete layouts
+			*/
+			return position;
 		},
-		// boundingBox: {
-		// 	x1: 200,
-		// 	y1: 100,
-		// 	w: 400,
-		// 	h: 300
-		// },
-		onStep: function (obj) {
-			// do nothing
+		zoom: 1, // zoom level as a positive number to set after animation
+		boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+		onStep: function ( obj ) {
+			/*
+			Callback when one step in simulated annealing is completed.
+			Object obj has many useful attributes
+			{
+				state: Current state,
+				energy: Energy of the current state,
+				temperature: Current temperature,
+				steps: Current step in the current iteration,
+				iterations: Current iteration
+			}
+			*/
 		},
-		zoom: 1,
-		animationDuration: 200,
-		fit: false, // If set to true will move every node even if you move one node, because it is trying to fit the graph.
-		SAInitialTemperature: 100000
+		SAInitialTemperature: 1000, // Initial temperature
+		SATemperatureDecreaseRate: 0.99, // Rate at which temperature decreases
+		nodeDistanceFactor: 0.1, // Node distance factor used to compute node distance cost.
+		borderDistanceFactor: 0.1, // Border distance factor used to compute border distance cost.
+		edgeLengthFactor: 0.5, // Edge length factor used to compute edge length cost.
+		nodeEdgeDistanceFactor: 0.1, // Node Edge distance factor used to compute node edge distance cost.
+		edgeCrossingsFactor: 10, // Edge crossings factor used to compute edge crossings cost.
+		iterations: 2, // Number of simulated annealing iterations
+		steps: 20, // Number of steps per iterations.
 	};
 
-	var extend = Object.assign || function (tgt) {
-		for (var i = 1; i < arguments.length; i++) {
-			var obj = arguments[i];
+	var extend = Object.assign || function ( tgt ) {
+		for ( var i = 1; i < arguments.length; i++ ) {
+			var obj = arguments[ i ];
 
-			for (var k in obj) {
-				tgt[k] = obj[k];
+			for ( var k in obj ) {
+				tgt[ k ] = obj[ k ];
 			}
 		}
 		return tgt;
@@ -48,16 +77,16 @@ var SALayout = function (cytoscape) {
 
 	// constructor
 	// options : object containing layout options
-	function Layout(options) {
-		this.options = extend({}, defaults, options);
+	function Layout( options ) {
+		this.options = extend( {}, defaults, options );
 	}
 
 	Layout.prototype.run = function () {
 		var layout = this;
-		layout.trigger({
+		layout.trigger( {
 			type: 'layoutstart',
 			layout: layout
-		});
+		} );
 		var options = this.options;
 		options.layout = layout;
 		var cy = options.cy; // cy is automatically populated for us in the constructor
@@ -66,36 +95,36 @@ var SALayout = function (cytoscape) {
 		var edges = eles.edges();
 
 		var bb = options.boundingBox ? options.boundingBox : cy.extent();
-		if (bb.x2 === undefined) {
+		if ( bb.x2 === undefined ) {
 			bb.x2 = bb.x1 + bb.w;
 		}
-		if (bb.w === undefined) {
+		if ( bb.w === undefined ) {
 			bb.w = bb.x2 - bb.x1;
 		}
-		if (bb.y2 === undefined) {
+		if ( bb.y2 === undefined ) {
 			bb.y2 = bb.y1 + bb.h;
 		}
-		if (bb.h === undefined) {
+		if ( bb.h === undefined ) {
 			bb.h = bb.y2 - bb.y1;
 		}
 
 		options.boundingBox = bb;
 		options.center = {
-			x: (bb.x1 + bb.x2) / 2,
-			y: (bb.y1 + bb.y2) / 2
+			x: ( bb.x1 + bb.x2 ) / 2,
+			y: ( bb.y1 + bb.y2 ) / 2
 		};
 
-		layout.trigger('layoutready');
+		layout.trigger( 'layoutready' );
 
-		var sa = SAGraph(options);
-		return sa.run().then(function (currentObject) {
-			layout.trigger('layoutstop');
+		var sa = SAGraph( options );
+		return sa.run().then( function ( currentObject ) {
+			layout.trigger( 'layoutstop' );
 			return layout;
-		});
+		} );
 	};
 
 	Layout.prototype.stop = function () {
-		this.trigger('layoutstop');
+		this.trigger( 'layoutstop' );
 
 		return this; // chaining
 	};
@@ -109,6 +138,6 @@ var SALayout = function (cytoscape) {
 
 };
 
-module.exports = function get(cytoscape) {
-	return SALayout(cytoscape);
+module.exports = function get( cytoscape ) {
+	return SALayout( cytoscape );
 };
